c4ef51df4d8b5fbdf3c2da2b1eded6e5e32a7166
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-02-25 18:01:21 -0300

    arreglo de error de auto-deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
