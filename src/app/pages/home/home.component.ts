cf676c6011b4a8ca5ec3e513943e29ebe371d369
JpCortezF <juanpactz@gmail.com>
2025-02-27 01:56:44 -0300

    Cambios en el navbar, footer y se agrego el carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CarouselComponent } from "../../components/home-components/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
