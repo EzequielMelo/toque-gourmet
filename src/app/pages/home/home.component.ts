99cd250420b29411d39d3db8c3de71a28ecd965f
JpCortezF <juanpactz@gmail.com>
2025-03-03 16:16:20 -0300

    Cards de servicios, form de contacto terminados.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from "../../components/home-components/carousel/carousel.component";
 import { CardComponent } from "../../components/home-components/card/card.component";
+import { ContactComponent } from "../../components/general-components/contact/contact.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselComponent, CardComponent],
+  imports: [CarouselComponent, CardComponent, ContactComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
