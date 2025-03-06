1ce51e5a966df184adca83398d517a3ed2a09e48
JpCortezF <juanpactz@gmail.com>
2025-03-06 01:14:47 -0300

    Page "Noticias" terminada,

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/general-components/header/header.component';
 import { SeparatorComponent } from "../../components/about-us-components/separator/separator.component";
+import { ServiceCategoryComponent } from '../../components/services-components/service-category/service-category.component';
+import { ServiceCategoryReverseComponent } from '../../components/services-components/service-category-reverse/service-category-reverse.component';
+import { ContactComponent } from "../../components/general-components/contact/contact.component";
 
 @Component({
   selector: 'app-about-us',
-  imports: [HeaderComponent, SeparatorComponent],
+  imports: [HeaderComponent, SeparatorComponent, ServiceCategoryComponent, ServiceCategoryReverseComponent, ContactComponent],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css',
 })
