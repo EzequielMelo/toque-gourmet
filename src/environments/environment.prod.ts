5a978b450c5a589182d6b7b14dd96d53c939a3e1
EzequielMelo <ezequielanthonymelo@gmail.com>
2025-03-28 02:47:38 -0300

    cambios para el modo de produccion

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'FIREBASE_PROJECT_ID',
+    appId: 'FIREBASE_APP_ID',
+    storageBucket: 'FIREBASE_STORAGE_BUCKET',
+    apiKey: 'FIREBASE_API_KEY',
+    authDomain: 'FIREBASE_AUTH_DOMAIN',
+    messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
+  },
+  emailJsConfig: {
+    serviceId: 'EMAILJS_SERVICE_ID',
+    templateId: 'EMAILJS_TEMPLATE_ID',
+    publicKey: 'EMAILJS_PUBLIC_KEY',
+  },
+};
